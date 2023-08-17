85fab3fb1d02d94ce965a2450a7b0b4a0b0a2e85
FEROZMEMON <ferozmemon726@gmail.com>
2023-08-17 13:04:51 +0500

    problem solving

---
+++ app.js
@@ -0,0 +1,105 @@
+// ASSIGNMENT 01 
+
+//  Q no = 01
+
+// const feroz = 30
+// const memon = 20
+// console.log(feroz + memon);
+
+
+
+//   Q no = 02
+
+// function namess(firstname, lastname) {
+//     const fullname = (firstname + lastname)
+//     console.log("salam" + " " + fullname);
+// }
+// const firstname = "feroz "
+// const lastname = "A Habib"
+// namess(firstname, lastname)
+
+
+
+
+// Q no = 03
+
+// function addition() {
+//     let num1 = +prompt("enter a number")
+//     let num2 = +prompt("enter a number")
+//     let sum =(num1+num2)
+//     return("the sum of "+ num1+" + "+num2+" is "+sum )    
+// }
+// console.log(addition());
+
+// Q no = 04
+
+// function calculator(num1, num2, operator) {
+
+//     let result 
+//     switch (operator) {
+//         case "+":
+//             result = num1 + num2
+//             break;
+//         case "-":
+//             result = num1 - num2
+//             break;
+//         case "*":
+//             result = num1 * num2
+//             break;
+//         case "/":
+//             result = num1 / num2
+//             break
+//         default:
+//           "invlaid operator";
+
+
+//     }
+//           return ("ans: " + num1 + " " + operator + " " + num2 + " = " + result )
+// }
+
+// let num1 = +prompt("enter number")
+// let num2 = +prompt("enter number")
+// let operator = prompt("enter an operator")
+
+// let result= calculator(num1,num2,operator)
+
+// console.log(result)
+
+// Q no = 05
+
+// function squareNumber(number) {
+//     return number*number
+     
+// }
+
+// let input = +prompt("enter number")
+
+// let result = squareNumber(input) 
+// console.log("The square of " +input+" is "+result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
